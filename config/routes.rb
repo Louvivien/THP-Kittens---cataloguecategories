a86006c4140f1caf62ebe72184933160241acc3a
mathieumllt <mathieu.maillet@ymail.com>
2018-11-26 14:14:53 +0100

    checkout page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'product/index'
+  get 'product/checkout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
