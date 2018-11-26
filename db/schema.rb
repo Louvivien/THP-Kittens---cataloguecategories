498f4ff9ca60696cd85e526279a38d49d2b4fecc
Juliette Audema <juliette.audema@gmail.com>
2018-11-26 17:56:06 +0100

    création order + ajouts colonnes total et subtotal dans cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_143848) do
+ActiveRecord::Schema.define(version: 2018_11_26_164922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2018_11_26_143848) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "total"
+    t.decimal "subtotal"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
@@ -39,6 +41,14 @@ ActiveRecord::Schema.define(version: 2018_11_26_143848) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,19 @@
+require 'faker'
+
 20.times do
   json = HTTParty.get('http://aws.random.cat/meow')
   Item.create("title": Faker::Cat.name, "description": Faker::Simpsons.quote, "price": rand(10)+1, "image_url": json.parsed_response["file"])
+end
+
+
+5.times do 
+  User.create(email: Faker::Internet.email, password: "truc965", password_confirmation: "truc965")
+end
+
+3.times do 
+  Cart.create(user_id: rand(5)+1)
+end 
+
+5.times do 
+  CartItem.create(cart_id: rand(3)+1, item_id: rand(20)+1, quantity: rand(10)+1)
 end
