3cc315cfcd7398c8f004052aca03470dfcbe1ece
Crysicia <lucienmontmartre@yahoo.fr>
2018-11-26 16:06:25 +0100

    Update order email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'thpstudent@gmail.com'
   layout 'mailer'
-  def test_email
-    @recipient = params[:user]
-    mail(to: @recipient.email, subject: 'Welcome to the jungle')
-  end
 end
