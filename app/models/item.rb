a940ca61537ed0aee8ce8f9d97120a661f3bb120
ssimonlp <simon.lepavec@gmail.com>
2018-11-26 13:35:30 +0100

    Email validations added

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: {greater_than: 0}, on: :create
   validates :title, presence: true, format: {with: /\A[a-zA-Z0-9+\s]+\z/, message: "Only alpha-numeric characters"}, length: { in: 3..30 }, on: :create
   validates :description, presence: true, length: {in: 10..500, message: "Between 10 and 500 characters"}, on: :create
-  validates :image_url, presence: true, format: {with: URI::regexp(["ftp", "http", "https"])}, on: :create
+  validates :image_url, presence: true, format: {with: URI::regexp(["http", "https"])}, on: :create
 end
