d7bc08f689742387affc8210010563643e5871e8
Crysicia <lucienmontmartre@yahoo.fr>
2018-11-27 14:07:33 +0100

    Add admin

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,7 @@
 class AdminMailer < ApplicationMailer
+  def user_order_email
+    @recipient = admin_user
+    @order = Order.last
+    mail(to: @recipient.email, subject: 'Thanks for ordering!')
+  end
 end
