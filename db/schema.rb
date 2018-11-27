c00fb57f8772458e58cdfac7b74e77967eccac35
mathieumllt <mathieu.maillet@ymail.com>
2018-11-27 16:36:22 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_27_140839) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,10 +67,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_140839) do
   create_table "orders", force: :cascade do |t|
     t.decimal "total"
     t.bigint "user_id"
-    t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
