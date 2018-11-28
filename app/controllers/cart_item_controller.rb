42d2381829cdb48991aa507bc3b178204eae0ce9
mathieumllt <mathieu.maillet@ymail.com>
2018-11-28 17:39:42 +0100

    space

--- app/controllers/cart_item_controller.rb
+++ app/controllers/cart_item_controller.rb
@@ -37,7 +37,7 @@
   end
   private
     def cart_item_params
-      params.require(:cart_item).permit(:quantity,:item_id, :cart_id)
+      params.require(:cart_item).permit(:quantity, :item_id, :cart_id)
     end
 end
 
