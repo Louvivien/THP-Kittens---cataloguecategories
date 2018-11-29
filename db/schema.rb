2ad08f5e307b6dc6b9fb2d93eb3c0b05ed35a31f
Juliette Audema <juliette.audema@gmail.com>
2018-11-29 18:09:21 +0100

    Catégories qui fonctionnent

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2018_11_29_145821) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "category"
     t.bigint "category_id"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
